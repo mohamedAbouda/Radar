38a4124ef6a1e1a02359a3b48c0011a63600013e
mohamed abouda <mohamed.aboda500@gmail.com>
2017-08-29 13:01:37 +0200

    change server name in listen

--- resources/views/dashboard/listen.blade.php
+++ resources/views/dashboard/listen.blade.php
@@ -6,8 +6,8 @@
 @section('scripts')
 <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/1.4.6/socket.io.min.js"></script>
 <script type="text/javascript">
-    //var socket = io('restart-technology.com:3000');
-    var socket   = io('http://localhost:3000');
+    var socket = io('restart-technology.com:3000');
+    //var socket   = io('http://localhost:3000');
     //getting the auth user id.
     var driverId =  {{\Auth::user()->id}};
     socket.on('location:'+ driverId, function (location) {
