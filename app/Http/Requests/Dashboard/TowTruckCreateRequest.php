35eefd4dbe1727080fe5efc36659c52243435015
maradney <maradney.dev@gmail.com>
2017-09-19 11:36:58 +0200

    [DASHBOARD] towtruck phone number validation

--- app/Http/Requests/Dashboard/TowTruckCreateRequest.php
+++ app/Http/Requests/Dashboard/TowTruckCreateRequest.php
@@ -25,7 +25,7 @@ class TowTruckCreateRequest extends FormRequest
     {
         return [
             'name' => 'required',
-            'phone' => 'required',
+            'phone' => 'required|regex:/^([\+0]([0-9]+[\- ]?)+)$/',
             'pic' => 'required|image'
         ];
     }
